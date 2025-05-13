8c32adf95c537eb31bce4566d162d926b8dea719
Jordin de León <jordindredev@gmail.com>
2025-05-13 11:27:53 -0600

    liquidar

--- app/Filament/Ventas/Resources/VentaResource.php
+++ app/Filament/Ventas/Resources/VentaResource.php
@@ -507,7 +507,7 @@ class VentaResource extends Resource implements HasShieldPermissions
                         ->slideOver()
                         ->stickyModalHeader()
                         ->modalSubmitAction(false),
-                    /* Action::make('nota_credito')
+                    Action::make('nota_credito')
                         ->icon('heroicon-o-document-arrow-down')
                         ->visible(fn ($record) => auth()->user()->can('credit_note', $record))
                         ->modalContent(fn (Venta $record): View => view(
@@ -522,7 +522,7 @@ class VentaResource extends Resource implements HasShieldPermissions
                         ->modalWidth(MaxWidth::FiveExtraLarge)
                         ->slideOver()
                         ->stickyModalHeader()
-                        ->modalSubmitAction(false),  */
+                        ->modalSubmitAction(false), 
                     /* Action::make('facturar')
                         ->label('Facturar')
                         ->requiresConfirmation()
--- app/Policies/VentaPolicy.php
+++ app/Policies/VentaPolicy.php
@@ -137,7 +137,7 @@ class VentaPolicy
 
     public function credit_note(User $user, Venta $venta): bool
     {
-        $estadosPermitidos = ['creada', 'liquidada', 'devuelta', 'parcialmente devuelta'];
+        $estadosPermitidos = ['devuelta', 'parcialmente devuelta'];
 
         return $user->can('credit_note_venta') && in_array($venta->estado->value, $estadosPermitidos);
     }
