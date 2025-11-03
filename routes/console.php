21c4d1650c416c335899715cdbd46eec1b6998b3
Jordin de León <jordindredev@gmail.com>
2025-11-03 12:13:12 -0600

    fix

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,5 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('filament:optimize')->everyMinute();
 Schedule::command('backup:clean')->daily()->at('00:20');
 Schedule::command('backup:run')->daily()->at('00:20');
