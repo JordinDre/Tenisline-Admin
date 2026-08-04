d511d409816dae438da483fdb9edb36b0358f161
Jordin de León <jordindeleon@MacBook-Pro-de-Jordin-2.local>
2026-08-04 01:05:46 -0600

    correcciones en backups

--- routes/console.php
+++ routes/console.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('backup:clean')->daily()->at('23:35');
 Schedule::command('backup:run --only-db')->daily()->at('23:35');
+Schedule::command('activitylog:clean')->daily()->at('23:45');
